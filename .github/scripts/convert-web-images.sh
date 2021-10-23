7bd648d0734168806b5852781eb74d3042c4eeb1
Benjamin Vickers <bvickers@vmware.com>
2021-10-23 14:12:14 +0200

    fix image conversion script

--- .github/scripts/convert-web-images.sh
+++ .github/scripts/convert-web-images.sh
@@ -5,4 +5,4 @@ IMG_DIR="$1"
 IMG_FILETYPE="$2"
 REPO_DIR="$( dirname "${BASH_SOURCE[0]}" )/../.."
 
-for img in $REPO_DIR/$IMG_DIR/*.$IMG_FILETYPE; do cwebp -metadata all -q 60 "${img}" -o "$(basename "${img}" .$IMG_FILETYPE).webp"; done
+for img in $REPO_DIR/$IMG_DIR/*.$IMG_FILETYPE; do cwebp -metadata all -q 60 "${img}" -o "$REPO_DIR/$IMG_DIR/$(basename "${img}" .$IMG_FILETYPE).webp"; done
